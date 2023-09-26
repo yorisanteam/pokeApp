2104a10aea47e4fbf7acc9ba38e127df0fc521ca
aoki <k.aoki@akatsuki.tokyo>
2023-09-26 19:57:37 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState(null);
+
+  const fetchPokemon = async () => {
+    const minNum = 1;
+    const maxNum = 1020;
+    let randNum = Math.floor(Math.random()*(maxNum+1-minNum)) + minNum;
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randNum}`);
+      const data = await response.json();
+      setPokemonData(data);
+    } catch (error) {
+      console.error('ポケモン情報の取得に失敗しました。', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPokemon();
+  },[]);
+
+  const handleButtonClick = () => {
+    fetchPokemon();
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>ポケアップ</h1>
+      <button onClick={handleButtonClick}>ポケモンを取得</button>
+      {pokemonData && (
+        <div>
+          <h2>{pokemonData.name}</h2>
+          <img src={pokemonData.sprites.front_default} alt={pokemonData.name}
+          />
+        </div>
+      )}
     </div>
   );
 }
